6118f87edac9bf7b07e74f29ac5b60f59640f2a3
Tandin Dorji <65913825+tandynhd@users.noreply.github.com>
2022-11-21 11:57:16 +0700

    .env update

--- components/firebase.js
+++ components/firebase.js
@@ -2,10 +2,10 @@
 import { initializeApp } from 'firebase/app';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-require("dotenv").config();
-console.log(process.env.apiKey);
+//require("dotenv").config();
+//console.log(process.env.apiKey);
 const firebaseConfig = {
-    apiKey: process.env.apiKey;
+    apiKey: "AIzaSyDoKHo90OlIpHHyi7yo9Ca9zBrhy8pYSqU",
     authDomain: "medapp-91193.firebaseapp.com",
     projectId: "medapp-91193",
     storageBucket: "medapp-91193.appspot.com",
